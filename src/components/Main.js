82ef3b2f31c06aeb44cdbf628accd6f688076d61
John KM Jung <min85da@gmail.com>
2017-02-24 18:06:53 -0500

    semantic ui setup

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import { Button } from 'semantic-ui-react'
 
-class Main extends React.Component {
+class Main extends Component {
   render(){
     return (
           <div className="main-container">
             <h1>Main.js loaded</h1>
+              <Button> Testing </Button>
           </div>
       )
   }
