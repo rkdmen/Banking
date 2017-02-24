d879dc56248564f4082879321a579fd540d42ed2
John KM Jung <min85da@gmail.com>
2017-02-24 17:50:08 -0500

    testing file setup

---
+++ tests/Main.spec.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { expect } from 'chai';
+
+import Main from '../src/components/Main';
+
+describe('<Main/>', function () {
+
+  it('it should have main-controller classname', function () {
+    const wrapper = shallow(<Main/>);
+    console.log(wrapper.debug());
+    expect(wrapper.contains(<div className="main-container" />));
+
+  });
+
+
+});
+
